300a58690d68f10fe40d303f8c2478439f2f3cbc
dungngo99 <dungngotanlhp@gmail.com>
2024-10-07 01:32:52 -0700

    Task 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -18,8 +20,17 @@ public class Main {
           // ensures that we don't run into 'Address already in use' errors
           serverSocket.setReuseAddress(true);
           // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-          clientSocket.getOutputStream().write("+PONG\r\n".getBytes(StandardCharsets.UTF_8));
+          while (!serverSocket.isClosed()) {
+              clientSocket = serverSocket.accept();
+              while (true) {
+                  BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                  String str = reader.readLine();
+                  if (str == null) {
+                      break;
+                  }
+                  clientSocket.getOutputStream().write("+PONG\r\n".getBytes(StandardCharsets.UTF_8));
+              }
+          }
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
